f048d814049a626249f828a3ec75c166bfeed1d5
sykoism <kenkoo611@gmail.com>
2021-06-14 15:43:50 +0800

    Update Template.php

--- Template.php
+++ Template.php
@@ -1,25 +1,3 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    
-    <title>KWH DDIR DSA Management System</title>
-    
     <?php include('inc/header.php');?>
-  </head>
-  
-  <body>
-    <!-- Bootstrap JS -->
-    <script src="js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-    
     <?php include('inc/container.php');?>
-    
     <?php include('inc/footer.php');?>
-  </body>
-
-</html>

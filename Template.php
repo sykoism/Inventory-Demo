751e898cd6c076562948aeb18d92c704e39e2333
sykoism <kenkoo611@gmail.com>
2021-06-14 15:48:08 +0800

    Update Template.php

--- Template.php
+++ Template.php
@@ -1,3 +1,13 @@
-    <?php include('inc/header.php');?>
-    <?php include('inc/container.php');?>
-    <?php include('inc/footer.php');?>
+<?php 
+session_start();
+include('inc/header.php');
+include 'Inventory.php';
+$inventory = new Inventory();
+$inventory->checkLogin();
+?>
+
+
+<?php include('inc/container.php');?>
+
+
+<?php include('inc/footer.php');?>
